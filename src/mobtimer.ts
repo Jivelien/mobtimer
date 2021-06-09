2830d6fb00cd2deb8bcdb3cb692fc329cdd23036
acassaigne <anthony.cassaigne@gmail.com>
2021-06-09 13:33:25 +0200

    it's working, with a different documentation.

--- src/mobtimer.ts
+++ src/mobtimer.ts
@@ -2,9 +2,24 @@ import express from "express";
 const app = express();
 const port = 8080 || process.env.PORT;
 
+var startTimer = new Date().getTime() / 1000 ;
+var endDate = new Date().getTime() / 1000 ;
+var duration = 4;
+
 app.get("/", (req, res) => {
   res.send("Hi!");
 });
+
+app.get("/start", (req, res) => {
+  startTimer = new Date();
+  endDate = startTimer + duration;
+  res.send(`start time ${startTimer}`);
+});
+
+app.get("/current", (req, res) => {
+  res.send("Hi!");
+});
+
 
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
