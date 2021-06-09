ce74ac20e6e513460fa377dc0ae860c49f670f63
Jivelien <julien.Delannoy@hotmail.com>
2021-06-09 13:43:29 +0200

    stash

--- src/mobtimer.ts
+++ src/mobtimer.ts
@@ -4,21 +4,25 @@ const port = 8080 || process.env.PORT;
 
 var startTimer = new Date().getTime() / 1000 ;
 var endDate = new Date().getTime() / 1000 ;
-var duration = 4;
+var duration = 10;
 
 app.get("/", (req, res) => {
   res.send("Hi!");
 });
 
 app.get("/start", (req, res) => {
-  startTimer = new Date();
+  startTimer = new Date().getTime() / 1000;
   endDate = startTimer + duration;
   res.send(`start time ${startTimer}`);
 });
 
 app.get("/current", (req, res) => {
-  res.send("Hi!");
+  var currentTimer = new Date().getTime() / 1000;
+  var isSliceTimeEnded = currentTimer > endDate
+  res.send(`end time ${endDate} - current time ${currentTimer} - is ended : ${isSliceTimeEnded} `);
 });
+
+
 
 
 app.listen(port, () => {
