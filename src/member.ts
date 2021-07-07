dc6041af1ffd0f06eb1b1f16ceb9b6b820648f82
Jivelien <julien.Delannoy@hotmail.com>
2021-07-07 13:23:55 +0200

    stash

---
+++ src/member.ts
@@ -0,0 +1,13 @@
+class Member {
+    lastname: string;
+  
+    constructor(lastname: string) {
+        this.lastname = lastname;
+    }
+  
+    getLastname() {
+        return "X"
+    }
+  }
+
+export {Member}
