e60fab39a506bdfcc42fb31976ce3a4638b2c039
acassaigne <anthony.cassaigne@gmail.com>
2021-10-21 13:51:48 +0200

    Comment add last test
    Co-authored-by: Jivelien <julien.Delannoy@hotmail.com>

--- src/member.ts
+++ src/member.ts
@@ -1,6 +1,6 @@
 export class Member {
     lastname: string;
-    next?: Member;
+    private next?: Member;
 
     constructor(lastname: string) {
         this.lastname = lastname;
@@ -9,13 +9,22 @@ export class Member {
     getLastname() {
         return this.lastname;
     }
+
+    getNext(): Member {
+        return <Member>this.next;
+    }
+
+    setNext(member: Member) {
+        this.next = member;
+    }
+
 }
 
 export class ListMembers {
     private firstmember?: Member;
-    private firstmember2 : Member;
-    private readonly sentinelle : Member;
-    private currentMember : Member;
+    private firstmember2: Member;
+    private readonly sentinelle: Member;
+    private currentMember: Member;
 
     constructor() {
         this.sentinelle = new Member("Sentinelle")
@@ -23,14 +32,14 @@ export class ListMembers {
         this.currentMember = this.firstmember2;
     }
 
-    add_at_last_postion(member: Member) {
-        let lastMember = this.get_last();
-        if (lastMember == null) {
-            this.firstmember = member
-            return
-        }
-        lastMember.next = member 
-    }
+    // add_at_last_postion(member: Member) {
+    //     let lastMember = this.get_last();
+    //     if (lastMember == null) {
+    //         this.firstmember = member
+    //         return
+    //     }
+    //     lastMember.setNext(member)
+    // }
 
     private is_empty_list(): boolean {
         return this.currentMember == this.sentinelle
@@ -38,26 +47,25 @@ export class ListMembers {
 
     add_at_first_postion(member: Member) {
 
-        member.next = this.firstmember2
+        member.setNext(this.firstmember2);
         this.firstmember2 = member
         if (this.is_empty_list())
             this.currentMember = this.firstmember2
     }
 
-    private get_last()  {
-        if (this.firstmember == null) {
-            return null;
-        }
-        else {
-            let currentMember = this.firstmember;
-
-            while (currentMember.next != null) {
-                currentMember = currentMember.next;
-            }
-
-            return currentMember;
-        }
-    }
+    // private get_last() {
+    //     if (this.firstmember == null) {
+    //         return null;
+    //     } else {
+    //         let currentMember = this.firstmember;
+    //
+    //         while (currentMember.next != null) {
+    //             currentMember = currentMember.next;
+    //         }
+    //
+    //         return currentMember;
+    //     }
+    // }
 
     // TODO : add_at_first_position
 
@@ -65,14 +73,15 @@ export class ListMembers {
         return this.firstmember;
     }
 
-    current2() : Member {
+    current2(): Member {
         return this.currentMember;
     }
 
     next() {
-        if (this.current2().next == null) {}
+        if (this.current2().getNext() == null) {
+        }
         // TODO next ?
-        return this.currentMember ;
+        return this.currentMember;
     }
 
     // next2() {
